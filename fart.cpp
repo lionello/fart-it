c3c7fe721280fdd7b442faf1b82a3d0e5d3ac6b0
Lionello Lunesu <lio+git@lunesu.com>
2023-10-09 16:48:19 -0700

    Update version to 1.99d

--- fart.cpp
+++ fart.cpp
@@ -49,7 +49,7 @@
 
 ///////////////////////////////////////////////////////////////////////////////
 
-#define VERSION				"v1.99c"
+#define VERSION				"v1.99d"
 
 #define _WILDCARD_SEPARATOR	','
 #define WILDCARD_ALL		"*"
